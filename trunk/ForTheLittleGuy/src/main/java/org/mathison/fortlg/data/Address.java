4f85fc3494e3ea1218402326a2e9aac9655de0af
daryl.mathison@gmail.com <daryl.mathison@gmail.com@a78b3e92-a2d7-721b-ade4-6f7cdb7e275b>
2013-08-08 04:06:56 +0000

    ForTheLittleGuy - added the rest of the entity beans and finished the InsertContacts. Fixed mistakes in the app-config.xml file so I don't need a persistence.xml file.

---
+++ trunk/ForTheLittleGuy/src/main/java/org/mathison/fortlg/data/Address.java
@@ -0,0 +1,142 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mathison.fortlg.data;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Version;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author daryl
+ */
+@Entity
+@Table(name = "ADDRESS")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Address.findAll", query = "SELECT a FROM Address a")})
+public class Address implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Column(name = "VERSION")
+    @Version
+    private Integer version;
+    @Column(name = "STREET")
+    private String street;
+    @Column(name = "CITY")
+    private String city;
+    @Column(name = "STATE")
+    private String state;
+    @JoinColumn(name = "CONTACTID", referencedColumnName = "ID")
+    @ManyToOne
+    private Contact contactid;
+    @JoinColumn(name = "ADDRESSTYPEID", referencedColumnName = "ID")
+    @ManyToOne
+    private Addresstype addresstypeid;
+
+    public Address() {
+    }
+
+    public Address(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Contact getContactid() {
+        return contactid;
+    }
+
+    public void setContactid(Contact contactid) {
+        this.contactid = contactid;
+    }
+
+    public Addresstype getAddresstypeid() {
+        return addresstypeid;
+    }
+
+    public void setAddresstypeid(Addresstype addresstypeid) {
+        this.addresstypeid = addresstypeid;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Address)) {
+            return false;
+        }
+        Address other = (Address) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "org.mathison.fortlg.data.Address[ id=" + id + " ]";
+    }
+    
+}
